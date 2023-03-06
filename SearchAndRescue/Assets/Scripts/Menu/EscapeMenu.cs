9159392b5131f372a10c14574b29fc97dfa4ac61
Thomas heal <81531490+timmytim101@users.noreply.github.com>
2023-03-06 13:24:40 -0500

    Added UI and Pause/Unpause for GL

---
+++ SearchAndRescue/Assets/Scripts/Menu/EscapeMenu.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EscapeMenu : MonoBehaviour
+{
+    public GameObject menuPanel; // The UI panel to be displayed when the Escape key is pressed
+
+    void Start()
+    {
+        // Hide the menu panel when the game starts
+        menuPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        // If the player presses the Escape key, toggle the menu panel on/off
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (menuPanel.activeSelf)
+            {
+                // If the menu panel is already active, hide it
+                menuPanel.SetActive(false);
+                //lock the cursor again
+                Cursor.lockState = CursorLockMode.Locked;
+                Time.timeScale = 1; // Resume the game
+            }
+            else
+            {
+                // If the menu panel is not active, show it
+                menuPanel.SetActive(true);
+                //Unlock the cursor
+                Cursor.lockState = CursorLockMode.None;
+                // Pause the game
+                Time.timeScale = 0;
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        //hide the menu panel and resume the game
+        menuPanel.SetActive(false);
+        //lock the cursor again
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1;
+    }
+}
