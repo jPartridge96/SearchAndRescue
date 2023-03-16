e1ae3a3db0729a162c881ccc6fb809a591d2843f
Thomas heal <81531490+timmytim101@users.noreply.github.com>
2023-03-16 09:14:47 -0400

    Update Door.cs

--- SearchAndRescue/Assets/Scripts/Interaction/Door.cs
+++ SearchAndRescue/Assets/Scripts/Interaction/Door.cs
@@ -2,21 +2,29 @@ using UnityEngine;
 
 public class Door : MonoBehaviour, IInteractable
 {
+    //angles for door
     public float openAngle = 90f;
     public float closeAngle = 0f;
+    //check if door is open
     private bool isOpen = false;
+    //quaternion for slerping door rotations
     private Quaternion openRotation;
     private Quaternion closeRotation;
+    //speed at which the door opens/closes
     public float smooth = 2.0f;
+    //the transform for the door game object (drag and drop)
     public Transform door;
 
+    //Changes the message for the string from the IInteractable interface
     public string Message
     {
         get { return isOpen ? "Close the door" : "Open the door"; }
     }
-
+    
+    
     private void Start()
     {
+	//Set the starting rotation of the door to the predefined rotation angles
         openRotation = door.rotation * Quaternion.Euler(0.0f, openAngle, 0.0f);
         closeRotation = door.rotation * Quaternion.Euler(0.0f, closeAngle, 0.0f);
     }
@@ -35,6 +43,7 @@ public class Door : MonoBehaviour, IInteractable
 
     public void Interact()
     {
+	//call the interact from the IInteractable, method is setup here to add sound or whatever in the Open/Close methods
         if (isOpen)
         {
             Close();
